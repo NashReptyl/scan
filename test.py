7d8417fb06978d784d71851592c2ab491a9fa9a3
Reptyl <poke.bref@gmail.com>
2023-05-29 20:45:31 +0200

    MongoDB ajout

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import tweepy
 import requests
 from bs4 import BeautifulSoup
 import time
+from pymongo import MongoClient
 
 # API Twitter variables d'access
 bearer=os.environ.get('BEARER')
@@ -10,6 +11,7 @@ consumer_key = os.environ.get('CONSUMER_KEY')
 consumer_secret = os.environ.get('CONSUMER_SECRET')
 access_token = os.environ.get('ACCESS_TOKEN')
 access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
+mdp = os.environ.get('DBMDP')
 
 # Creation client API
 api = tweepy.Client(bearer_token=bearer,
@@ -17,12 +19,18 @@ api = tweepy.Client(bearer_token=bearer,
                     access_token_secret=access_token_secret,
                     consumer_key=consumer_key,
                     consumer_secret=consumer_secret)
+
+# Connexion à la base de données MongoDB
+client = MongoClient('mongodb+srv://Reptyl:'+mdp+'@scannews.ovnlnjy.mongodb.net/?retryWrites=true&w=majority')
+db = client['ScanNews']
+collection = db['tweets']
+
 LIST_MANGAS = ["Eternal Club",
                "Ao No Hako",
                "Forget Vivian"]
 
 # Fonction pour tweeter les scans
-def tweet_latest_japscan(j):
+def tweet_latest_japscan():
     # Récupération des scans à partir du site japscan
     news_url = 'https://www.japscan.lol/'
     response = requests.get(news_url)
@@ -33,8 +41,7 @@ def tweet_latest_japscan(j):
     scan_div = container.find_all('div', class_='py-1')
 
     # Vérification des derniers scans tweetés
-    with open('scans_tweetes.txt', 'r', encoding='utf-8') as file:
-        scans_tweetes = [line.strip() for line in file.readlines()]
+    scans_tweetes = [scan['titre']+'/'+scan['chapitre'] for scan in collection.find()]
 
     # Publier uniquement les nouveaux scans
     for scan in scan_div:
@@ -48,7 +55,7 @@ def tweet_latest_japscan(j):
                 api.create_tweet(text='Le '+chapitre.lower()+' de '+titre+' est sorti !')
                                 
                 # Ajouter le scan à la liste des scans publiés
-                scans_tweetes.append(titre+'/'+chapitre)
+                collection.insert_one({'titre': titre, 'chapitre': chapitre})
 
     # Enregistrer les scans dans le fichier
     with open('scans_tweetes.txt', 'w',encoding='utf-8') as file:
@@ -58,9 +65,6 @@ def tweet_latest_japscan(j):
 
 
 # Exécuter en boucle
-n=1
-tweet_latest_japscan(n)
-#while True:
-#    tweet_latest_japscan(n)
-#    n=n+1
-#    time.sleep(60) 
+while True:
+    tweet_latest_japscan()
+    time.sleep(60) 
